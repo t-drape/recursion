66e2a170ada134e63302493c3acc0f0e34937203
t-drape <thebatreview1@gmail.com>
2024-08-27 19:38:30 -0700

    Finish recursive fibonacci

---
+++ fibonacci.rb
@@ -0,0 +1,30 @@
+def fibs(num)
+  arr = []
+  num.times do |time|
+    case arr.length
+    when 0
+      arr << 0
+    when 1
+      arr << 1
+    else
+      arr << (arr[time-1] + arr[time-2])
+    end
+  end
+  arr
+end
+
+p fibs(8)
+
+def fib(spot, arr=[], original_spot=spot)
+  if spot == 1
+    num = 0
+  elsif spot == 2
+    num = 1
+  else
+   num = fib(spot-1, arr, original_spot) + fib(spot-2, arr, original_spot)
+  end
+  arr[spot-1] = num unless arr[spot-1] 
+  return arr.length == original_spot ? arr : num
+end
+
+p fib(8)
